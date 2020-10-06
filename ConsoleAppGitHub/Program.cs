62f93a69080ba3e06809517353378a9b61f702a0
Dorensky <64221942+Dorensky@users.noreply.github.com>
2020-10-06 16:41:12 -0400

    Deuxieme test

--- ConsoleAppGitHub/Program.cs
+++ ConsoleAppGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Test vers le github distant");
+            Console.WriteLine("Deuxieme Test");
             Console.WriteLine("Hello World!");
         }
     }
